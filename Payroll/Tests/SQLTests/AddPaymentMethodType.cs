298dda8896ebeb1c8f2ad79eec22e248d35cb99e
Alex <alex.steed88@gmail.com>
2021-07-07 16:02:18 +0900

    Added Mail payment method.

--- Payroll/Tests/SQLTests/AddPaymentMethodType.cs
+++ Payroll/Tests/SQLTests/AddPaymentMethodType.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Data;
 using Mono.Data.Sqlite;
 using NUnit.Framework;
@@ -9,9 +10,46 @@ namespace Payroll.Tests
         [SetUp]
         public void ClearPaymentMethods()
         {
-            SqliteCommand clearPM = new SqliteCommand("DELETE FROM DirectDepositAccount", con);
+            ClearTable(SqliteDB.Tables.account);
+            ClearTable(SqliteDB.Tables.mail);
+        }
+
+        private void ClearTable(string tableName)
+        {
+            SqliteCommand clearPM = new SqliteCommand("DELETE FROM "+tableName, con);
             clearPM.ExecuteNonQuery();
         }
+
+        private DataTable GetDataTable(string Table)
+        {
+            string getAccounts = "SELECT * FROM "+Table;
+            SqliteCommand cmd = new SqliteCommand(getAccounts, con);
+            SqliteDataAdapter adapter = new SqliteDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            return table;
+        }
+
+   
+
+        [Test]
+        public void EmployeeCannotHaveMoreThanOnePM()
+        {
+            AccountGetsSaved();
+            try
+            {
+                MailPayGetsSaved();
+                Assert.Fail("Should not be able to add the same employee with a different payment method");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Assert.AreEqual(1, GetDataTable(SqliteDB.Tables.account).Rows.Count);
+            Assert.AreEqual(0, GetDataTable(SqliteDB.Tables.mail).Rows.Count,"Mail should not get added since employee failed");
+        }
+
         [Test]
         public void HoldPaymentSaved()
         {
@@ -23,46 +61,26 @@ namespace Payroll.Tests
         [Test]
         public void AccountGetsSaved()
         {
-           
-            
             int accountNumber = 12315;
-            addEmployeeMethod(new AccountPaymentMethod("New bank",accountNumber));
+            addEmployeeMethod(new AccountPaymentMethod("New bank", accountNumber));
             string expectedCode = SqliteDB.PaymentMethods.Account;
             CompareSavedScheduleType(expectedCode);
-            DataTable accountsTable = GetAccountsTable();
-            Assert.AreEqual(1,accountsTable.Rows.Count);
+            DataTable accountsTable = GetDataTable(SqliteDB.Tables.account);
+            Assert.AreEqual(1, accountsTable.Rows.Count);
             DataRow row = accountsTable.Rows[0];
-            Assert.AreEqual(id,row["EmpID"]);
+            Assert.AreEqual(id, row["EmpID"]);
             Assert.AreEqual("New bank", row["Bank"]);
             Assert.AreEqual(accountNumber, row["Account"]);
         }
 
-        private DataTable GetAccountsTable()
-        {
-            string getAccounts = "SELECT * FROM DirectDepositAccount";
-            SqliteCommand cmd = new SqliteCommand(getAccounts, con);
-            SqliteDataAdapter adapter = new SqliteDataAdapter(cmd);
-            DataTable table = new DataTable();
-            adapter.Fill(table);
-            return table;
-        }  
-        private DataTable GetPaycheAddressTable()
-        {
-            string getAccounts = "SELECT * FROM PaycheckAddress";
-            SqliteCommand cmd = new SqliteCommand(getAccounts, con);
-            SqliteDataAdapter adapter = new SqliteDataAdapter(cmd);
-            DataTable table = new DataTable();
-            adapter.Fill(table);
-            return table;
-        }
 
         [Test]
-        public void PostSaved()
+        public void MailPayGetsSaved()
         {
             addEmployeeMethod(new MailPaymentMethod("home"));
-            string expectedCode = SqliteDB.PaymentMethods.Post;
+            string expectedCode = SqliteDB.PaymentMethods.Mail;
             CompareSavedScheduleType(expectedCode);
-            DataTable paycheckAddresses = GetPaycheAddressTable();
+            DataTable paycheckAddresses = GetDataTable(SqliteDB.Tables.mail);
             Assert.AreEqual(1, paycheckAddresses.Rows.Count);
             DataRow row = paycheckAddresses.Rows[0];
             Assert.AreEqual(id, row["EmpID"]);
