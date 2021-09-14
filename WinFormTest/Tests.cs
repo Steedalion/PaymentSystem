4a490c514245c333d36106fd70972374d7dff52d
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-09-14 15:28:36 +0900

    Added winforms GUI for add employee transaction.

---
+++ WinFormTest/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using WindowsFormsUI;
+
+namespace WinFormTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            AddEmployeeTransationForm window = new AddEmployeeTransationForm();
+            window.empIDTb.Text = 123 + "";
+        }
+    }
+}
---
+++ WindowsFormsUI/AddEmployeeTransationForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsUI
+{
+    public partial class AddEmployeeTransationForm : Form
+    {
+        public AddEmployeeTransationForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void empIdUpdated(object sender, EventArgs e)
+        {
+            NumberOnly(empIDTb);
+        }
+
+
+        private void addressBox_TextChanged(object sender, EventArgs e)
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        private void AddEmployeeTransationForm_Load(object sender, EventArgs e)
+        {
+            EnterState(AddEmployeeStates.Hourly);
+            // throw new System.NotImplementedException();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void commisionedRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            EnterState(AddEmployeeStates.Commisioned);
+        }
+
+        private void salariedRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            EnterState(AddEmployeeStates.Salaried);
+        }
+
+        private void hourlyRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            EnterState(AddEmployeeStates.Hourly);
+        }
+
+        private void NumberOnly(TextBox textBox, char seperator = '0')
+        {
+            textBox.Text=new string(textBox.Text.Where(c => char.IsDigit(c) || c==seperator).ToArray());
+        }
+
+        private void EnterState(AddEmployeeStates state)
+        {
+            HourlyRateTextBox.ReadOnly = state != AddEmployeeStates.Hourly;
+            SalariedSalaryTextBox.ReadOnly = state != AddEmployeeStates.Salaried;
+            CommsionedRateBox.ReadOnly = state != AddEmployeeStates.Commisioned;
+            CommisionedSalaryBox.ReadOnly = state != AddEmployeeStates.Commisioned;
+        }
+
+        private void HourlyRateTextBox_TextChanged(object sender, EventArgs e)
+        {
+            NumberOnly(HourlyRateTextBox,'.');
+        }
+
+        private void SalariedSalaryTextBox_TextChanged(object sender, EventArgs e)
+        {
+            NumberOnly(SalariedSalaryTextBox,'.');
+        }
+
+        private void CommsionedRateBox_TextChanged(object sender, EventArgs e)
+        {
+            NumberOnly(CommsionedRateBox,'.');
+        }
+
+        private void CommisionedSalaryBox_TextChanged(object sender, EventArgs e)
+        {
+NumberOnly(CommisionedSalaryBox,'.');        }
+    }
+
+    internal enum AddEmployeeStates
+    {
+        Hourly,
+        Salaried,
+        Commisioned
+    }
+}
